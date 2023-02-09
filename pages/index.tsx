43238595927859ca8362973038189e940b784ff5
mfahadarif85 <mfahadarif@gmail.com>
2023-02-09 08:51:32 +0500

    details section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
-const IndexPage = () => <h1>Hello Next.JS</h1>;
+import Cart from "@/src/sections/Cart";
+import Details from "@/src/sections/Details";
+import { Container, Flex, VStack } from "@chakra-ui/react";
+
+const IndexPage = () => (
+  <Container maxW='container.xl' p={0}>
+    <Flex h='100vh' py={20}>
+      <Details />
+      <Cart />
+    </Flex>
+  </Container>
+);
 
 export default IndexPage;
