fae5f915497746de094dbfb3f8f8d254e1ceafa4
mfahadarif85 <mfahadarif@gmail.com>
2023-02-09 17:26:15 +0500

    reponsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import { Container, Flex, VStack } from "@chakra-ui/react";
 
 const IndexPage = () => (
   <Container maxW='container.xl' p={0}>
-    <Flex h='100vh' py={20}>
+    <Flex
+      h={{ base: "auto", md: "100vh" }}
+      py={[0, 10, 20]}
+      direction={{ base: "column-reverse", md: "row" }}
+    >
       <Details />
       <Cart />
     </Flex>
